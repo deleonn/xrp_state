d8e848096f462a2435ea8e5a86dd8dc25f0ea5e9
Joel de Leon <joel.deleon@onephase.com>
2019-11-29 00:51:19 -0600

    Add coinmarketcap api to get XRP prices

--- src/server.ts
+++ src/server.ts
@@ -9,15 +9,27 @@ app.get('/', (req, res) => {
   res.send('Hello XRP!');
 });
 
-app.get('/value', () => {
-  fetch('https://api.coinmarketcap.com/v1/ticker/', {
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then(res => {
-    console.log(res.body);
-  });
+const qs = '?symbol=XRP&convert=MXN';
+
+app.get('/value', async () => {
+  const value = await fetch(
+    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest${qs}`,
+    {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CMC_PRO_API_KEY': '94f06b4e-0d1b-4177-8eec-067ad81384df',
+      },
+    }
+  )
+    .then(res => res.json())
+    .then(res => {
+      return res.data;
+    })
+    .catch(err => console.log(err));
+
+  console.log(JSON.stringify(value));
+  return value;
 });
 
 app.listen(process.env.PORT || PORT, () => {
